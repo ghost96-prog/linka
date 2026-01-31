a83444d81465b1861952c60468e3663d68d1d719
iamghost <gkmangezi09@email.com>
2026-01-31 14:15:05 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // This should be '/' for Vercel
 })
