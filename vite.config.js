e4d57ae5465bcaebbb6a224bc060394e42bc52ae
iamghost <gkmangezi09@email.com>
2026-01-31 14:51:42 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // This should be '/' for Vercel
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Linka',
+        short_name: 'Linka',
+        theme_color: '#0ea5e9',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  base: '/',
 })
